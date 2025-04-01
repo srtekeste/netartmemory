4a55b31a0c75cd5b4006d693d10b9ec26965bcfd
srtekeste <srtekeste@dons.usfca.edu>
2025-04-01 11:36:27 -0700

    1136

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     let z = 10;
+//     function bringToFront(el) {
+//       el.style.zIndex = ++z;
+//     }
+//     clickableImages.forEach(img => {
+//       img.addEventListener("click", () => bringToFront(img));
+//     });
+//   });
