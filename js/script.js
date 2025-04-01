62c09032e2284f6d6cafbb60f78d6dc0437f9165
srtekeste <srtekeste@dons.usfca.edu>
2025-04-01 12:27:22 -0700

    1227

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@
 //       img.addEventListener("click", () => bringToFront(img));
 //     });
 //   });
+
+  window.addEventListener("DOMContentLoaded", () => {
+    const img = document.getElementById("slide-img");
+  
+    // Delay the slide in by 100ms for a smoother effect
+    setTimeout(() => {
+      img.classList.add("slide-in");
+    }, 100); 
+  
+    // Optional: slide out after 10s
+    setTimeout(() => {
+      img.classList.remove("slide-in");
+    }, 10100);
+  });
